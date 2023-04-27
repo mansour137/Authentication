e651bf3163791734f321b70b9d80be11dbb3d614
Mansour Sayed <mansoursayed446@gmail.com>
2023-04-27 19:11:38 +0200

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     email:String,
     password:String,
 })
-
+console.log(process.env.SECRET)
 userSchema.plugin(encrypt,{secret : process.env.SECRET , encryptedFields:['password']})
 
 
